32d05ed588bb5d135d5f7f0efd0cfdefe1153171
Sarah Maulani <sarahmaulani73@gmail.com>
2025-12-29 01:08:49 +0700

    Update admin.py

--- spk/admin.py
+++ spk/admin.py
@@ -10,10 +10,7 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 @admin.register(Periode)
 class PeriodeAdmin(admin.ModelAdmin):
-    list_display = ['nama', 'tanggal_mulai', 'tanggal_selesai', 'is_active', 'is_current']
-    list_filter = ['is_active', 'tanggal_mulai']
-    list_editable = ['is_active']
-    search_fields = ['nama']
+    list_display = ['nama_periode', 'tahun']
 
 @admin.register(Produk)
 class ProdukAdmin(admin.ModelAdmin):
