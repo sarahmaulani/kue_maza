3edb83c00f68625ff6ce7867599e044704938314
Sarah Maulani <sarahmaulani73@gmail.com>
2025-12-29 09:13:15 +0000

    Update wsgi.py

--- spk_maza/wsgi.py
+++ spk_maza/wsgi.py
@@ -8,12 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-import spk.init_admin
 
 
 from django.core.wsgi import get_wsgi_application
 
-spk.init_admin.run()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spk_maza.settings')
 
