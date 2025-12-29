f84c41a22d440a082b788a34bf79498ed3f3b552
Sarah Maulani <sarahmaulani73@gmail.com>
2025-12-29 10:03:08 +0000

    Update 0002_create_admin.py

--- spk/migrations/0002_create_admin.py
+++ spk/migrations/0002_create_admin.py
@@ -2,12 +2,11 @@ from django.db import migrations
 
 def create_admin(apps, schema_editor):
     User = apps.get_model('auth', 'User')
-
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser(
             username='admin',
-            email='admin@gmail.com',
-            password='admin123'
+            password='admin123',
+            email='admin@example.com'
         )
 
 class Migration(migrations.Migration):
