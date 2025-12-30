9c8c4eabe2f0e395e7ff2dbb0d729b88191c7278
Sarah Maulani <sarahmaulani73@gmail.com>
2025-12-30 10:38:43 +0700

    Update models.py

--- spk/models.py
+++ spk/models.py
@@ -15,7 +15,7 @@ class UserProfile(models.Model):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='viewer')
     # phone = models.CharField(max_length=20, blank=True)
     # telepon = models.CharField(max_length=20, blank=True, default='')
-    alamat = models.CharField(max_length=255)
+    alamat = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     #mo nampilin username sama roleny
