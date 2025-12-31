a16eadf364c15fc076514001ff7398b72be3ccc9
Sarah Maulani <sarahmaulani73@gmail.com>
2025-12-31 15:47:52 +0700

    Update views.py

--- spk/views.py
+++ spk/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.cache import never_cache
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
