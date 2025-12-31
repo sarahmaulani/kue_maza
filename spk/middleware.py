1f85f6511e598c4709b46d883877a29056211409
Sarah Maulani <sarahmaulani73@gmail.com>
2025-12-31 15:37:08 +0700

    Create middleware.py

---
+++ spk/middleware.py
@@ -0,0 +1,18 @@
+# spk/middleware.py
+class NoCacheMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        
+        # Add no-cache headers untuk semua protected pages
+        if request.user.is_authenticated:
+            response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+            response['Pragma'] = 'no-cache'
+            response['Expires'] = '0'
+        
+        # Prevent back button cache
+        response['X-Accel-Expires'] = '0'
+        
+        return response
